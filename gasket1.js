ffe4e13a343bc9ab8e07ad31e3c69a2eef79a489
Dharmik79 <dholariyadharmik7@gmail.com>
2022-11-13 14:05:22 -0600

    added different color cubes in the canvas

--- gasket1.js
+++ gasket1.js
@@ -16,14 +16,17 @@ window.onload = function init() {
   var program = initShaders(gl, "vertex-shader", "fragment-shader");
   gl.useProgram(program);
 
-  const bufferData = new Float32Array([0, 0,0,1, 300]);
+  const bufferData = new Float32Array([
+    0, 0, 0, 1, 300, 1, 0, 0, 0.5, -0.8, 0, 1, 32, 0, 1, 0, -0.9, 0.5, 0, 1, 50,
+    0, 0, 1,
+  ]);
   const aPositionLoc = gl.getAttribLocation(program, "aPosition");
   const aPointSizeLoc = gl.getAttribLocation(program, "aPointSize");
-
+  const aColorLoc = gl.getAttribLocation(program, "aColor");
   // To enable the vertex array
   gl.enableVertexAttribArray(aPositionLoc);
   gl.enableVertexAttribArray(aPointSizeLoc);
-
+  gl.enableVertexAttribArray(aColorLoc);
 
   // Create a buffer to hold the data points
   const buffer = gl.createBuffer();
@@ -31,14 +34,14 @@ window.onload = function init() {
   gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
   gl.bufferData(gl.ARRAY_BUFFER, bufferData, gl.STATIC_DRAW);
 
-  gl.vertexAttribPointer(aPositionLoc, 4, gl.FLOAT, false, 4 * 5, 0);
-  gl.vertexAttribPointer(aPointSizeLoc, 1, gl.FLOAT, false, 4 * 5, 4 * 4);
-
+  gl.vertexAttribPointer(aPositionLoc, 4, gl.FLOAT, false, 4 * 8, 0);
+  gl.vertexAttribPointer(aPointSizeLoc, 1, gl.FLOAT, false, 4 * 8, 4 * 4);
+  gl.vertexAttribPointer(aColorLoc, 3, gl.FLOAT, false, 4 * 8, 4 * 5);
   // Render the data points.
   render();
 };
 
 function render() {
   // To draw the graphics on the page
-  gl.drawArrays(gl.POINTS, 0, 1);
+  gl.drawArrays(gl.POINTS, 0, 3);
 }
