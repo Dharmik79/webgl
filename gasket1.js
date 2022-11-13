c67bea2ad81f723ffdc57d9f38d6fcb9a9ab9194
Dharmik7 <dholariyadharmik7@gmail.com>
2022-11-13 17:41:36 -0600

    draw Elements added

--- gasket1.js
+++ gasket1.js
@@ -16,38 +16,58 @@ window.onload = function init() {
   var program = initShaders(gl, "vertex-shader", "fragment-shader");
   gl.useProgram(program);
 
-  const elementVertexData = new Float32Array([
-    0,0,  0,0,0,
-    0,1,    1,0,0,
-    0.95106,0.30902, 0,1,0,
-    0.58779,-.80902,0,0,1,
-    -.58779,-.80902,1,1,0,
-    -.95106,0.30902,1,0,1
+  const arrayVertexData = new Float32Array([
+    0,0,				1,0,0,
+    0.00000,1.00000,	1,0,0,
+    0.95106,0.30902,	1,0,0,
+  
+    0,0,				0,1,0,
+    0.95106,0.30902,	0,1,0,
+    0.58779,-.80902,	0,1,0,
+  
+    0,0,				0,0,1,
+    0.58779,-.80902,	0,0,1,
+    -.58779,-.80902,	0,0,1,
+  
+    0,0,				1,1,0,
+    -.58779,-.80902,	1,1,0,
+    -.95106,0.30902,	1,1,0,
+  
+    0,0,				1,0,1,
+    -.95106,0.30902,	1,0,1,
+    0.00000,1.00000,	1,0,1,
   ]);
-
-  const elementIndexData=new Float32Array([
+  
+  const elementVertexData = new Float32Array([
+    0,0,				0,0,0,
+    0.00000,1.00000,	1,0,0,
+    0.95106,0.30902,	0,1,0,
+    0.58779,-.80902,	0,0,1,
+    -.58779,-.80902,	1,1,0,
+    -.95106,0.30902,	1,0,1,
+  ]);
+  
+  const elementIndexData = new Uint8Array([
     0,1,2,
     0,2,3,
     0,3,4,
     0,4,5,
-    0,5,1
-  ])
-  const aPositionLoc = gl.getAttribLocation(program, "aPosition");
-  const aColorLoc = gl.getAttribLocation(program, "aColor");
-  
- 
+    0,5,1,
+  ]);
+  const aPositionLoc=gl.getAttribLocation(program,"aPosition");
+  const aColorLoc=gl.getAttribLocation(program,"aColor");
 
+  const arrayVertexBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, arrayVertexBuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, arrayVertexData, gl.STATIC_DRAW);
   
-  // Create a buffer to hold the data points
-  const buffer = gl.createBuffer();
-  // Binding the buffer to the program
-  gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
-  gl.bufferData(gl.ARRAY_BUFFER, elementVertexData, gl.STATIC_DRAW);
-
-  const elementIndexBuffer = gl.createBuffer();
-  // Binding the buffer to the program
-  gl.bindBuffer(gl.ARRAY_BUFFER, elementIndexBuffer);
-  gl.bufferData(gl.ARRAY_BUFFER, elementIndexData, gl.STATIC_DRAW);
+  // const elementVertexBuffer = gl.createBuffer();
+  // gl.bindBuffer(gl.ARRAY_BUFFER, elementVertexBuffer);
+  // gl.bufferData(gl.ARRAY_BUFFER, elementVertexData, gl.STATIC_DRAW);
+  
+  // const elementIndexBuffer = gl.createBuffer();
+  // gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, elementIndexBuffer);
+  // gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, elementIndexData, gl.STATIC_DRAW);
 
   gl.vertexAttribPointer(aPositionLoc, 2, gl.FLOAT, false, 5*4, 0);
   gl.vertexAttribPointer(aColorLoc, 3, gl.FLOAT, false, 5*4, 2*4);
@@ -59,7 +79,7 @@ window.onload = function init() {
 
 function render() {
   // To draw the graphics on the page
-//   gl.drawArrays(gl.TRIANGLES, 0, 15);
+   gl.drawArrays(gl.TRIANGLES, 0, 15);
 
-gl.drawElements(gl.TRIANGLES,15,gl.UNSIGNED_BYTE,0)
+// gl.drawElements(gl.TRIANGLES,15,gl.UNSIGNED_BYTE,0)
 }
